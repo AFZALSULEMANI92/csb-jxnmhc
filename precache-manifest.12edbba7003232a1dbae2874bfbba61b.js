cf3f73c7bd749f4899d39165c2332339ebcec6a3
Code Sandbox <hello@codesandbox.io>
2022-03-13 10:09:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.12edbba7003232a1dbae2874bfbba61b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50020f2a4fa1a2917c59c91ae330ad75",
+    "url": "https://afzalsulemani92.github.io/csb-jxnmhc/index.html"
+  },
+  {
+    "revision": "bcf2a5f21878e7a4bb72",
+    "url": "https://afzalsulemani92.github.io/csb-jxnmhc/static/js/2.7bd492ba.chunk.js"
+  },
+  {
+    "revision": "34c84a8ba27ea8361f78",
+    "url": "https://afzalsulemani92.github.io/csb-jxnmhc/static/js/main.1a1a733a.chunk.js"
+  },
+  {
+    "revision": "1c2b32e4c9ebb152778e",
+    "url": "https://afzalsulemani92.github.io/csb-jxnmhc/static/js/runtime-main.4e649ee3.js"
+  }
+]);
